80923bd4f58fe63f2aec93e4f1bdc87c6e238ea8
itskarudo <itskarudo@protonmail.com>
2023-06-02 16:58:24 +0100

    AAAAAAAA

--- main/urls.py
+++ main/urls.py
@@ -3,26 +3,15 @@ from . import views
 
 urlpatterns = [
 
-		path("register",views.register,name="register"),
-		path("login",views.loginPage,name="login"),
-		path("logout",views.logoutPage,name="logout"),
         path("index", views.index, name="index"),
-   		path("wiki/<str:title>",views.entry,name="wiki-entry"),
-  		path("search",views.search,name="search"),
-    	path("search_results/<str:query>/<str:results>",views.search_results,name="search_results"),
    		path("",views.home,name="home"),
         path("about",views.about,name="about"),
         path("contact",views.contact,name="contact"),
-        path("wiki/<str:title>",views.entry,name="wiki-entry"),
         path("error",views.error,name="error"),
         path("layout",views.layout,name="layout"),
         path("course",views.course,name="course"),
-        path("instal",views.instal,name="instal"),
-        path("doc",views.doc,name="doc"),
-        path("origine",views.origine,name="origine"),
-      # path("mai",views.mai,name="mai"),
-      # path("success",views.success,name="success"),
-        path("Logil",views.Logil,name="Logil"),
+        path("install",views.instal,name="instal"),
+        path("docs",views.doc,name="doc"),
 
   
 ]	
