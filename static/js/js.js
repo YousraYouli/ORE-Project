6011f238212bcd4847517f1f38d455858d640a71
itskarudo <itskarudo@protonmail.com>
2023-06-04 11:47:16 +0100

    add hamburger menu back

--- static/js/js.js
+++ static/js/js.js
@@ -1,3 +1,15 @@
+let navbar = document.querySelector(".header .navbar");
+// menu bnt active when we use phone mode
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.add("active");
+};
+
+document.querySelector("#close-navbar").onclick = () => {
+  navbar.classList.remove("active");
+};
+
+let acountform = document.querySelector(".account-form");
 //  swiper user reviews
 
 var swiper = new Swiper(".reviews-slider", {
